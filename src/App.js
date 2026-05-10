cd3d58879b242762bf562f1dd143b93d679172f0
bazan yaroslav <bazan_ak24@nuwm.edu.ua>
2026-05-11 01:55:15 +0300

    Виконано реалізацію веб-застосування

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Question from "./components/Question";
+import qBank from "./components/QuestionBank";
+import Score from "./components/Score";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            questionBank: qBank,
+            currentQuestion: 0,
+            selectedOption: "",
+            score: 0,
+            quizEnd: false,
+        };
+    }
+
+    handleOptionChange = (e) => {
+        this.setState({ selectedOption: e.target.value });
+    };
+
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        this.checkAnswer();
+        this.handleNextQuestion();
+    };
+
+    checkAnswer = () => {
+        const { questionBank, currentQuestion, selectedOption } = this.state;
+        if (selectedOption === questionBank[currentQuestion].answer) {
+            this.setState((prevState) => ({ score: prevState.score + 1 }));
+        }
+    };
+
+    handleNextQuestion = () => {
+        const { questionBank, currentQuestion } = this.state;
+        if (currentQuestion + 1 < questionBank.length) {
+            this.setState((prevState) => ({
+                currentQuestion: prevState.currentQuestion + 1,
+                selectedOption: "",
+            }));
+        } else {
+            this.setState({
+                quizEnd: true,
+            });
+        }
+    };
+
+    render() {
+        const { questionBank, currentQuestion, selectedOption, score, quizEnd } =
+            this.state;
+        return (
+            /* align-items-start притискає вміст до лівого краю, p-5 додає відступи від стінок */
+            <div className="App d-flex flex-column align-items-start justify-content-center p-5">
+                <h1 className="app-title">Тест на знання основ React.js</h1>
+                {!quizEnd ? (
+                    <Question
+                        question={questionBank[currentQuestion]}
+                        selectedOption={selectedOption}
+                        onOptionChange={this.handleOptionChange}
+                        onSubmit={this.handleFormSubmit}
+                    />
+                ) : (
+                    <Score
+                        score={score}
+                        className="score"
+                    />
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
